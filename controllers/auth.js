c27475bad3f85a5d26ca5141533df10fed12f9e7
hackybacky <hacky3244@gmail.com>
2022-08-08 16:54:07 +0530

    jwttokenadded

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs"
+import { createError } from "../Error.js";
 export const signup = async (req, res,next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
@@ -12,3 +13,16 @@ export const signup = async (req, res,next) => {
     next(err);
   }
 };
+
+export const signin = async(req,res,next)=>{
+  try{
+    
+    const user = await User.findOne({name:req.body.name})
+    if(!user)return next(createError(404,"User not found"));
+    const isCorrect =await bcrypt.compare(req.body.password,user.password);
+    if(!isCorrect)return next(createError(400,"Wrong name or password"));
+    res.status(200).json(user);
+  }catch(err){
+    next(err);
+  }
+}
